8b6d5fb4791855aedb1f451857a937444a598949
cqr <cqr@intergal.coop>
2022-02-23 17:46:20 +0100

    nuevo resource con userupdate

--- src/main/java/coop/intergal/AppConst.java
+++ src/main/java/coop/intergal/AppConst.java
@@ -91,7 +91,7 @@ public class AppConst {   // @@ TODO this class for now is copy and represents t
 	public static final String FORMAT_FOR_DATETIME_FOR_JOIN = "TO_TIMESTAMP_TZ%28'#value#','YYYY-MM-DD%22T%22hh24:mi:ss'%29"; // is use in Oracle for JOIN as the parent data includes T by example 2021-01-13T17:26:36 , left empty for Mysql
 	public static final String FORMAT_FOR_DATETIME = "TO_TIMESTAMP_TZ%28'#value#','YYYY-MM-DD hh24:mi:ss'%29"; // is use in Oracle (when you write a date with time in Query Form you don't use T  left empty for Mysql
 	public static final String FORMAT_FOR_SHORTDATETIME = "TO_TIMESTAMP_TZ%28'#value#','YYYY-MM-DD'%29"; // is use in Oracle, left empty for Mysql
-	public static final String RESOURCES_WITH_USER = "CR-PED_PROVEED_CAB";
+	public static final String RESOURCES_WITH_USER = "CR-ARTICULOS,CR-PED_PROVEED_CAB";
 
 //	public static final String FORMAT_FOR_DATETIME = "TO_TIMESTAMP_TZ('#value#','YYYY-MM-DD\"T\"hh24:mi:ss')"; // is use in Oracle, left empty for Mysql
 	
